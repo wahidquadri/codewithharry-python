0083aa93c95be4f860bc5472913825693f5f9ef5
wahidquadri <dhurwahidquadri@Dhurs-MacBook-Air.local>
2024-02-22 20:12:57 +0530

    fir commit

---
+++ conditional statement/twoo.py
@@ -0,0 +1,15 @@
+# write a program to input 2 numbers and print their sum
+
+val1 = int(input("enter the value1 :"))
+val2 = int(input("enter the value2 :"))
+sum = val1+val2
+print(val1+val2)
+
+# write a program to input 2 num a,b print true if a is greater than or equal to b.if not print false
+
+a = int(input("first num :"))
+b = int(input("second num :"))
+if(a>=b):
+    print("true")
+else:
+    print("false")
