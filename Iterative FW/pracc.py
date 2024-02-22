0b96ac70d25e82a69ea105ebdd7d9b2de1cec829
wahidquadri <dhurwahidquadri@Dhurs-MacBook-Air.local>
2024-02-22 23:33:39 +0530

    first commit

---
+++ Iterative FW/pracc.py
@@ -0,0 +1,69 @@
+# Print numbers from 1 to 10 using a for loop ?
+'''for i in range(1,11):
+    print(i)'''
+
+# Print numbers from 1 to 10 using a While loop ?
+'''i=1
+while i<11:
+    print(i)
+    i=i+1'''
+
+#print 15 to 10 num using for loop 
+'''for i in range(15,9,-1):
+    print(i)'''
+
+#print 15 to 10 num using while loop
+'''i = 15
+while i>=10:
+    print(i)
+    i = i-1'''
+
+#print the first 10 even numbers using a while loop?
+'''i=2
+while i<=21:
+    print(i)
+    i = i+2'''
+
+
+# Program to print the first 5 even numbers using a for loop?
+'''for i in range(2,11,2):
+    print(i)
+'''
+
+# Program to print the first 10 odd numbers using a while loop?
+'''i = 1
+while i<=20:
+    print(i)
+    i = i+2
+'''
+
+#Program to print the first 10 odd numbers using a for loop
+'''for i in range (1,20,2):
+    print(i)'''
+
+#Program first ten multiples of 4 using a for loop(4,8,12,16...)?
+'''for i in range(1, 11):
+    multiple = 4 * i
+    print(multiple)'''
+
+##Program first ten multiples of 4 using a while loop(4,8,12,16...)?
+'''i = 1
+while i<=10:
+    multiple = 4 * i
+    print(multiple)
+    i = i+1
+'''
+
+#Program to print a multiplication table of 6 using a for loop.
+'''for i in range(1,11):
+    multiple = 6 * i
+    print(multiple)'''
+
+##Program to print a multiplication table of 6 using a for loop.
+'''i=1
+while i<=10:
+    multiple = 6 * i
+    print(multiple)
+    i = i+1
+'''
+
