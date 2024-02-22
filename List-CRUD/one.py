eea80aaac43ae7b2e52dca50741a20e1e79cb8f2
wahidquadri <dhurwahidquadri@Dhurs-MacBook-Air.local>
2024-02-23 01:06:41 +0530

    first commit

---
+++ List-CRUD/one.py
@@ -0,0 +1,66 @@
+#  l = [10,20,20,10,"vahid","khan",20.23,True,]
+
+#l.pop()  it will remove the last value in a given list 
+
+#l.reverse() it will reverse the all values in a given list
+
+# l.remove("khan") it wil remove the selected value in the given list
+
+# l.clear() it will clear all the data in a given list
+
+# l.append("Python") it will add the given new value at the end of the list
+
+# l.insert(2,"coding") it will insert the new data in a alloted place of the list
+
+'''x = l.count(20)  it will count the duplicates values in the given list
+print(x)'''
+
+# print(len(l)) # it tells the length of the above list
+# print(l)
+
+
+# Practise Question on List[]
+''' i am created my own question
+    for example list is [10,20,"vahid",True]
+    > in the above list add any numeric value and place it in a 3indexing place
+    > remove the last value in the list 
+    > make the above list in reverse
+    > remove "Vahid" in the list
+    > clear all the data in a list
+    > add any value at the end of the list
+    > finally count the duplicate value in the list '''
+
+
+mylist = [10,20,20,20,20,"vahid",True]
+
+'''mylist.insert(3,"khan")
+print(mylist)'''
+
+'''mylist.remove(True)
+print(mylist)'''
+
+'''mylist.reverse()
+print(mylist)'''
+
+'''mylist.remove("vahid")
+print(mylist)'''
+
+'''mylist.clear()
+print(mylist)'''
+
+'''mylist.append("DHUR")
+print(mylist)'''
+
+'''x = mylist.count(20)
+print(x)'''
+
+
+
+
+
+
+
+
+
+
+
