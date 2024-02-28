d534525a74c17074b950795b56bff407c13515d0
wahidquadri <dhurwahidquadri@Dhurs-MacBook-Air.local>
2024-02-28 17:44:24 +0530

    first commit

---
+++ nested/one.py
@@ -0,0 +1,91 @@
+# printing of 1 to 5 num in step by step way
+'''
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()
+
+'''
+
+# printing 5 stars 
+
+'''
+num_stars = 5
+print("*"*num_stars)
+
+'''
+
+# Print square pattern with 'A' symbols.
+'''
+num = 4
+for i in range (num):
+    for j in range(num):
+        print("A",end="")
+    print()
+    '''
+
+# Print square pattern with '*' symbols.
+'''
+num = 4
+for i in range(num):
+    for i in range(num):
+        print("*",end="")
+    print()
+'''
+
+# print square pattern with given number 5 digit
+'''
+num = 5
+for i in range(num):
+    for j in range(num):
+        print("5",end="")
+    print()
+
+'''
+
+#Print square pattern with a given fixed digit in every ROW ?
+'''
+for i in range(1, 5):  # Assuming you want a 4x4 square
+    for j in range(4):
+        print(i, end=' ')
+    print()
+
+'''
+
+# Print square pattern with given digit in descending order.
+'''
+for i in range(5,0,-1):
+    for j in range(5):
+        print(i,end="")
+    print()
+'''
+
+#Print Right Angle Triangle pattern with '*' symbol.
+'''
+*
+* *
+* * *
+* * * *
+'''
+'''
+for i in range(1,5):
+    for j in range(5):
+        print("*",end="")
+    print()
+'''
+
+# Print Reverse Right Angle Triangle Pattern with ‘*’ Symbol
+'''
+* * * * *
+* * * *
+* * *
+* *
+*
+'''
+
+'''
+for i in range(5,0,-1):
+    for j in range(i):
+        print("*",end="")
+    print()
+'''
